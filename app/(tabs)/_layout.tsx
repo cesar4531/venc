02d67ade13baaf94017ac2b2139066a0114ca287
cesar4531 <cesaradrianrs660@gmail.com>
2025-05-01 22:28:10 -0600

    update _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -40,6 +40,15 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+
+    <Tabs.Screen
+    name='news'
+    options={{
+      title:'News',
+      tabBarIcon:({color}) => <IconSymbol size={28} name='newspaper.fill' color={color}/>
+      
+    }}
+    />
     </Tabs>
   );
 }
